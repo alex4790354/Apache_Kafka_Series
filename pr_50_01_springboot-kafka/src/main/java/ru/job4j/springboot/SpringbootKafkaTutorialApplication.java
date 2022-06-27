eba80fbefe3774c3df61be88f537f496ede896a3
Alex4790354 <9720560@gmail.com>
2022-06-27 18:32:05 +0300

    C-11 Start SpringBoot_Kafka

---
+++ pr_50_01_springboot-kafka/src/main/java/ru/job4j/springboot/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+	}
+
+}
