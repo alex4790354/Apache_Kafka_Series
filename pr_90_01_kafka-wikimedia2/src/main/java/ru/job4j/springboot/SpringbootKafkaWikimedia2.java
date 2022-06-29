c42b991908a490b6f03ac84371860211ef8860ac
Alex4790354 <9720560@gmail.com>
2022-06-29 20:36:20 +0300

    C-14 Add new project: create new Wekimedia Kafka producer.

---
+++ pr_90_01_kafka-wikimedia2/src/main/java/ru/job4j/springboot/SpringbootKafkaWikimedia2.java
@@ -0,0 +1,13 @@
+package ru.job4j.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaWikimedia2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaWikimedia2.class, args);
+	}
+
+}
---
+++ pr_90_01_kafka-wikimedia2/src/test/java/ru/job4j/springboot/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
